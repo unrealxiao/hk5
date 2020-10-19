1da08d0ac57920c172d70e9640932cea678651c7
mac <mac@149-161-171-251.dhcp-bl.indiana.edu>
2020-10-19 17:54:09 -0400

    llr2.0

--- llr_functions.R
+++ llr_functions.R
@@ -6,9 +6,9 @@ llr <- function(x,y,z,omega){
 }
 
 compute_f_hat <- function(z,x,y,omega){
-  Wz <- make_weight_matrix(z,x,omega)
+  Wz <- diag(make_weight_matrix(z,x,omega))
   X <- make_predictor_matrix(x)
-  f_hat = c(1,z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
+  f_hat = c(1,z) %*% solve(t(X) %*% apply(X, 2, "*", Wz)) %*% t(X) %*% (Wz * y)
   return(f_hat)
 }
 
